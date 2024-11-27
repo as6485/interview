6252d36f040de36d1ae48ecba185eb611ce4f5f1
Ayan Sengupta <as6485@gmail.com>
2024-11-27 19:02:32 +0530

    leet progress 27th Nov 24

--- src/leetcode/MergeKSortedArrays.java
+++ src/leetcode/MergeKSortedArrays.java
@@ -62,13 +62,63 @@ public class MergeKSortedArrays {
 
     public static void main(String[] args)
     {
-
+        /*solution for above HeapNode
         int[][] arr = { { 2, 6, 12 },
                 { 1, 9 },
                 { 23, 34, 90, 2000 } };
         System.out.println(
                 MergeKSortedArrays.mergeKArrays(arr, arr.length)
-                        .toString());
+                        .toString());*/
+
+        //solution using ListNode below
+        ListNode l1_five = new ListNode(5);
+        ListNode l1_four = new ListNode(4, l1_five);
+        ListNode l1_one = new ListNode(1, l1_four);
+
+        ListNode l2_four = new ListNode(4);
+        ListNode l2_three = new ListNode(3, l2_four);
+        ListNode l2_one = new ListNode(1, l2_three);
+
+        ListNode l3_six = new ListNode(6);
+        ListNode l3_two = new ListNode(2, l3_six);
+
+        ListNode[] lists = {l1_one, l2_one, l3_two};
+
+        ListNode node = mergeKLists(lists);
+        while(node != null){
+            System.out.print(node.val+"->");
+            node = node.next;
+        }
+
+    }
+
+    public static ListNode mergeKLists(ListNode[] lists) {
+        if (lists==null || lists.length==0) return null;
+        //Note the pq only stores and compares the first node of each list
+        PriorityQueue<ListNode> queue= new PriorityQueue<ListNode>(lists.length, (a,b)-> a.val-b.val);
+
+        //dummy head
+        ListNode dummy = new ListNode(0);
+
+        //tracks the tail at all time
+        ListNode tail=dummy;
+
+        //store the first node of each list
+        for (ListNode node:lists)
+            if (node!=null)
+                queue.add(node);
+
+        while (!queue.isEmpty()){
+            //the min element among the first node of each list
+            tail.next=queue.poll();
+            //increment tail
+            tail=tail.next;
+            //insert the next element of the same list into the PQ
+            if (tail.next!=null)
+                queue.add(tail.next);
+        }
+        //dummy next is the start of the actual result list
+        return dummy.next;
     }
 }
 
