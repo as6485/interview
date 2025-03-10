41f16e4b61657b0c4ef745ac6b9a68b75bf89e1d
Ayan Sengupta <as6485@gmail.com>
2025-03-10 21:20:27 +0530

    BinaryTreeLevelOrderTraversal

--- src/leetcode/BinaryTreeLevelOrderTraversal.java
+++ src/leetcode/BinaryTreeLevelOrderTraversal.java
@@ -1,21 +1,49 @@
 package leetcode;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class BinaryTreeLevelOrderTraversal {
 
     public static void main(String[] args) {
         TreeNode t4 = new TreeNode(4);
         TreeNode t5 = new TreeNode(5);
+        TreeNode t6 = new TreeNode(6);
         TreeNode t7 = new TreeNode(7);
-        TreeNode t3 = new TreeNode(3, t7, null);
+        TreeNode t3 = new TreeNode(3, t6, t7);
         TreeNode t2 = new TreeNode(2, t4, t5);
         TreeNode t1 = new TreeNode(1, t2, t3);
 
-        System.out.println(levelOrder(t1));
+        //System.out.println(levelOrder(t1));
+        System.out.println(levelOrderBFS(t1));
     }
 
+    //BFS
+    public static List<List<Integer>> levelOrderBFS(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        List<List<Integer>> result = new LinkedList<List<Integer>>();
+
+        if(root == null) return result;
+
+        queue.offer(root);
+
+        while(!queue.isEmpty()){
+            int qLen = queue.size();
+            List<Integer> currentList = new LinkedList<Integer>();
+            for(int i=0; i<qLen; i++) {
+                if(queue.peek().left != null) queue.offer(queue.peek().left);
+                if(queue.peek().right != null) queue.offer(queue.peek().right);
+                currentList.add(queue.poll().val);
+            }
+            result.add(currentList);
+        }
+        return result;
+    }
+
+
+    //DFS
     private static List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> result = new ArrayList<>();
         int level = 0;
