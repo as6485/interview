f6e01c6af1bd8432531db3a1e1636aa25c2fda59
Ayan Sengupta <as6485@gmail.com>
2025-04-09 21:56:50 +0530

    9th April 2025

---
+++ src/leetcode/FindAllDuplicatesInAnArray.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static leetcode.LongestIncreasingSubsequence.lengthOfLIS;
+
+public class FindAllDuplicatesInAnArray {
+    public static void main(String[] args) {
+        int[] nums = {4,3,2,7,8,2,3,1};
+        System.out.println(findDuplicates(nums));
+    }
+    public static List<Integer> findDuplicates(int[] nums) {
+        List<Integer> ans = new ArrayList<>();
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            int x = Math.abs(nums[i]);
+            if (nums[x - 1] < 0) {
+                ans.add(x);
+            }
+            nums[x - 1] *= -1;
+        }
+        return ans;
+    }
+}
